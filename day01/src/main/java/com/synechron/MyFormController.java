120440bb01d633da39514b248069bd8cfd02616d
prabhu-durasoft <iamprabhu@yahoo.com>
2018-12-06 15:38:55 +0530

    mvc

--- day01/src/main/java/com/synechron/MyFormController.java
+++ day01/src/main/java/com/synechron/MyFormController.java
@@ -1,11 +1,22 @@
 package com.synechron;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyFormController {
 
+	@PostMapping("/process")
+	public String processName(@RequestParam String name, HttpSession session) {
+		String message = "Hello " + name;
+		session.setAttribute("message", message);
+		return "result";
+	}
+	
 	@GetMapping("/begin")
 	public String begin() {
 		return "index";
--- day01/src/main/webapp/index.jsp
+++ day01/src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
 	<h2>Welcome to index.jsp file</h2>
+	<form method="POST" action="/process">
+		<input type="text" name="name">
+		<button>Click</button>
+	</form>
 </body>
 </html>
